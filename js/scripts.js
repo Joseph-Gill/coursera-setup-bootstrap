9e0f07d24b75656c883dd22d9206e482a9e807a0
Joseph Edwin Gill <josephedwingill@gmail.com>
2020-10-22 16:16:57 +0200

    Grunt pt2

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,14 @@
 $(document).ready(function () {
-    const myCarousel = $('#mycarousel');
-    const carouselButton = $('#carouselButton');
-    myCarousel.carousel({interval: 2000});
-    carouselButton.click(function () {
-        if (carouselButton.children('span').hasClass('fa-pause')) {
-            myCarousel.carousel('pause');
-            carouselButton.children('span').removeClass('fa-pause');
-            carouselButton.children('span').addClass('fa-play');
+    $('#mycarousel').carousel({interval: 2000});
+    $('#carouselButton').click(function () {
+        if ($('#carouselButton').children('span').hasClass('fa-pause')) {
+            $('#mycarousel').carousel('pause');
+            $('#carouselButton').children('span').removeClass('fa-pause');
+            $('#carouselButton').children('span').addClass('fa-play');
         } else {
-            myCarousel.carousel('cycle');
-            carouselButton.children('span').removeClass('fa-play');
-            carouselButton.children('span').addClass('fa-pause');
+            $('#mycarousel').carousel('cycle');
+            $('#carouselButton').children('span').removeClass('fa-play');
+            $('#carouselButton').children('span').addClass('fa-pause');
         }
     });
     $('#loginButton').click(function () {
